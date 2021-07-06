8e27abb65ec86a1e71106467248226dd298a0be1
Richard Hancock <richardl@hancock1.com>
2021-07-06 03:44:17 -0600

    updated route part 2

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch, Route, Redirect} from 'react-router-dom';
+import {Switch, Route} from 'react-router-dom';
 import Home from './Components/Home';
 import About from './Components/About';
 import Projects from './Components/Projects';
